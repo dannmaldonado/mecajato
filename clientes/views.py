35c9fa98a904e0d0871f84717bad6a6a9acef89a
Danilo Maldonado <dannsanto@gmail.com>
2023-07-25 11:48:47 -0300

    atualizar clientes

--- clientes/views.py
+++ clientes/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import Cliente, Carro
 import re
+from django.core import serializers
+import json
 
 
 def clientes(request):
@@ -39,3 +41,11 @@ def clientes(request):
             car.save()
 
         return HttpResponse('Teste')
+
+
+def att_cliente(request):
+    id_cliente = request.POST.get('id_cliente')
+    cliente = Cliente.objects.filter(id=id_cliente)
+    cliente_json = json.loads(serializers.serialize('json', cliente))[
+        0]['fields']
+    return JsonResponse(cliente_json)
